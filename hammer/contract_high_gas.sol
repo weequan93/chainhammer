d57e1a675c19c0cc94a4595bf5bf77a520981c9f
weequan <wee@hyperlegend.onmicrosoft.com>
2025-03-19 22:23:05 +0800

    1

--- hammer/contract_high_gas.sol
+++ hammer/contract_high_gas.sol
@@ -7,12 +7,16 @@ contract simplestorage {
     uint256 public storedData;
     uint256 public num;
 
-    // 1m gas 25000
-    // 2m gas 25000
-    // 3m gas 25000
-    uint256 public a = 25000;
+    // a=70000  gas =  50,000,000
+    // a=20000 gas = 20000
+    // a=50000  gas =  30,000,000
+    // a=10000  gas =  gas=6,000,000
+    // a=1500 gas=1,000,000
+    // a=3300  gas=2,000,000
+    // a =5000, gas=3,000,000
+    uint256 public a = 5000;
 
-    function set(uint256 x) {
+    function set() external {
         // try failing transactions:
         // assert ( 1 == 0 );  // uses up all 90000 given gas
         // revert();           // uses 41686 gas
