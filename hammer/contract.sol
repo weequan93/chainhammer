fb2e1cfc92948a2920c9d8ff946dee694205c1a2
weequan <wee@hyperlegend.onmicrosoft.com>
2025-03-19 22:26:41 +0800

    1

--- hammer/contract.sol
+++ hammer/contract.sol
@@ -4,20 +4,31 @@
 pragma solidity ^0.4.21;
 
 contract simplestorage {
-  uint public storedData;
+    uint256 public storedData;
+    uint256 public num;
 
-  function set(uint x) {
-    storedData = x;        // uses ~26691 gas
-    
-    // try failing transactions:
-    // assert ( 1 == 0 );  // uses up all 90000 given gas
-    // revert();           // uses 41686 gas
-    // throw;              // same as revert(); 
-    // require ( 1 == 0 ); // uses 41714 gas
-  }
+    // a=70000  gas =  50,000,000
+    // a=20000 gas = 20000
+    // a=50000  gas =  30,000,000
+    // a=10000  gas =  gas=6,000,000
+    // a=1500 gas=1,000,000
+    // a=3300  gas=2,000,000
+    // a =5000, gas=3,000,000
+    uint256 public a = 5000;
 
-  function get() constant returns (uint retVal) {
-    return storedData;
-  }
+    function set() external {
+        // try failing transactions:
+        // assert ( 1 == 0 );  // uses up all 90000 given gas
+        // revert();           // uses 41686 gas
+        // throw;              // same as revert();
+        // require ( 1 == 0 ); // uses 41714 gas
+
+        for (uint256 i = 0; i < a; i++) {
+            num += a;
+        }
+    }
+
+    function get() view returns (uint256 retVal) {
+        return storedData;
+    }
 }
-
