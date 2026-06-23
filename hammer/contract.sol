4eae22dbd3c6b028eff5a0d1c45eb736b8b14f02
weequan <wee@hyperlegend.onmicrosoft.com>
2026-06-23 23:02:19 +0800

    feat

--- hammer/contract.sol
+++ hammer/contract.sol
@@ -14,7 +14,7 @@ contract simplestorage {
     // a=1500 gas=1,000,000
     // a=3300  gas=2,000,000
     // a =5000, gas=3,000,000
-    uint256 public a = 5000;
+    uint256 public a = 3300;
     
     function updateNum(uint256 _a) external {
         a = _a;
